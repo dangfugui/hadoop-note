85296305f5c49b1eca28b4be096ab2f3a75a88a1
dang <dangfugui@163.com>
2017-05-07 23:07:28 +0800

    update

--- src/main/java/com/dang/hdfs/Note.java
+++ src/main/java/com/dang/hdfs/Note.java
@@ -16,26 +16,26 @@ import java.util.Map;
 public class Note {
 
     public static void main(String []strs) throws Exception {
-        Configuration conf=new Configuration();
-
-        //也可以在代码中对conf中的配置信息进行手动设置，会覆盖掉配置文件中的读取的值
-        //设置文件系统为hdfs。如果设置这个参数，那么下面的方法里的路径就不用写hdfs://hello110:9000/
-        //conf.set("fs.defaultFS", "hdfs://hello110:9000/");
-
-        //根据配置信息，去获取一个具体文件系统的客户端操作实例对象
-        FileSystem.get(new URI("hdfs://localhost:9000/"), conf,"hadoop");//hdfs://localhost:9000
-
-        Iterator<Map.Entry<String, String>> iterator = conf.iterator();
-        while(iterator.hasNext()){
-            Map.Entry<String, String> ent=iterator.next();
-            System.out.println(ent.getKey()+"  :  "+ent.getValue());
-        }
-
-        System.out.println("-----------------/hdfs-site.xml 文件读取结束----------------");
-//        String HDFS_PATH = "hdfs://localhost:9000/user/hadoop/hello";
-//        URL.setURLStreamHandlerFactory(new FsUrlStreamHandlerFactory());
-//        final URL url = new URL(HDFS_PATH);
-//        IOUtils.copyBytes(url.openStream(),System.out,1024,true);
+//        Configuration conf=new Configuration();
+//
+//        //也可以在代码中对conf中的配置信息进行手动设置，会覆盖掉配置文件中的读取的值
+//        //设置文件系统为hdfs。如果设置这个参数，那么下面的方法里的路径就不用写hdfs://hello110:9000/
+//        //conf.set("fs.defaultFS", "hdfs://hello110:9000/");
+//
+//        //根据配置信息，去获取一个具体文件系统的客户端操作实例对象
+//        FileSystem.get(new URI("hdfs://localhost:9000/"), conf,"hadoop");//hdfs://localhost:9000
+//
+//        Iterator<Map.Entry<String, String>> iterator = conf.iterator();
+//        while(iterator.hasNext()){
+//            Map.Entry<String, String> ent=iterator.next();
+//            System.out.println(ent.getKey()+"  :  "+ent.getValue());
+//        }
+//
+//        System.out.println("-----------------/hdfs-site.xml 文件读取结束----------------");
+        String HDFS_PATH = "hdfs://localhost:9000/user/hadoop/hello";
+        URL.setURLStreamHandlerFactory(new FsUrlStreamHandlerFactory());
+        final URL url = new URL(HDFS_PATH);
+        IOUtils.copyBytes(url.openStream(),System.out,1024,true);
 //        //fileSystem();
 
 
